be38eadce442acde0da71303db9983b6255234ab
rkdwo <rkdwotn09@gmail.com>
2019-12-06 10:14:37 +0900

    19/12/06 ddd

--- kangjaesu.hotel/src/main/java/kangjaesu/hotel/user/service/UserService.java
+++ kangjaesu.hotel/src/main/java/kangjaesu/hotel/user/service/UserService.java
@@ -7,7 +7,7 @@ import kangjaesu.hotel.user.domain.User;
 public interface UserService {
 	List<User> listUsers();
 	List<User> searchListUsers(User user);
-	User findUser(User user);
+	User getUser(User user);
 	boolean join(User user);
 	//boolean cerrectUser(User user);
 	//boolean secede(int userNo);
