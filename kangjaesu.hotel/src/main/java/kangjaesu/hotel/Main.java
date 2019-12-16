55ec8dae52d7a1abf3cacdaf84bb394f0723f5b1
rkdwo <rkdwotn09@gmail.com>
2019-12-16 13:41:07 +0900

    546

--- kangjaesu.hotel/src/main/java/kangjaesu/hotel/Main.java
+++ kangjaesu.hotel/src/main/java/kangjaesu/hotel/Main.java
@@ -2,13 +2,16 @@ package kangjaesu.hotel;
 
 import javax.servlet.http.HttpSession;
 
+import kangjaesu.hotel.user.domain.User;
+import oracle.net.aso.s;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Main{
 	@RequestMapping("/")
-	public String main(){
+	public String main(HttpSession session){
 		return "main";
 	}
 	
